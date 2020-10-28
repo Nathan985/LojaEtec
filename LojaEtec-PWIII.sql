1e882a62195027431c583584894fce11626c78a4
NATHANPEREIRACAVALCA <nathan.rodrigu3s@gmail.com>
2020-10-27 23:18:59 -0300

    Padrão

---
+++ LojaEtec-PWIII.sql
@@ -0,0 +1,77 @@
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- CRIAÇÃO DO BANCO DE DADOS
+DROP DATABASE IF EXISTS LojaEtec;
+
+CREATE DATABASE IF NOT EXISTS LojaEtec;
+
+USE LojaEtec;
+
+-- CRIAÇÃO DE TABELAS
+
+#CRIAÇÃO DA TABELA USUARIOS
+CREATE TABLE IF NOT EXISTS Tbl_Usuario(
+
+	id_Usuario INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(20) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+	senha VARCHAR(60) NOT NULL,
+    foto VARCHAR(150) UNIQUE NOT NULL DEFAULT  'default_user.png',
+    nickname VARCHAR(50) UNIQUE NOT NULL,
+    ativo BOOL NOT NULL DEFAULT TRUE,
+    PRIMARY KEY(id_Usuario)
+);
+
+#CRIAÇÃO DA TABELA PRODUTOS
+CREATE TABLE IF NOT EXISTS Tbl_Produto(
+
+	id_Produto INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    codigo VARCHAR(40) NOT NULL UNIQUE,
+    nome VARCHAR(20) NOT NULL,
+	preco DECIMAL(10, 2) UNSIGNED NOT NULL,
+    foto VARCHAR(150) UNIQUE NOT NULL DEFAULT  'default_produto.png',
+    descircao LONGTEXT NULL,
+    PRIMARY KEY(id_Produto)
+);
+
+#CRIAÇÃO DA TABELA ESTOQUES
+CREATE TABLE IF NOT EXISTS Tbl_Estoque(
+
+	codigo VARCHAR(40) NOT NULL UNIQUE,
+    quantidade INT UNSIGNED NOT NULL,
+    data_estoque DATE NOT NULL DEFAULT NOW()
+
+);
+
+#CRIAÇÃO DA TABELA VENDAS
+CREATE TABLE IF NOT EXISTS Tbl_Vendas(
+
+	id_Venda INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    codigo VARCHAR(40) NOT NULL UNIQUE,
+    data_venda DATE NOT NULL DEFAULT NOW(),
+    total_venda DECIMAL(10, 2) UNSIGNED NOT NULL,
+    cpf VARCHAR(20) NULL,
+    pagamento VARCHAR(20) NOT NULL,
+	fk_id_Usuario INT UNSIGNED NOT NULL,
+    PRIMARY KEY(id_Venda)
+);
+
+#CRIAÇÃO DA TABELA QUANTIDADE_PRODUTOS
+CREATE TABLE IF NOT EXISTS Tbl_QuantidadeProdutos(
+
+	id_ProdutosComprados INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	quantidade INT UNSIGNED NOT NULL,
+    preco_unidade DECIMAL(10, 2) UNSIGNED NOT NULL,
+    total DECIMAL(10, 2) UNSIGNED NOT NULL,
+    fk_id_Venda INT UNSIGNED NOT NULL,
+    PRIMARY KEY(id_ProdutosComprados)
+);
+
+-- CRIANDO AS FOREIGN KEY
+
+#FOREIGN KEY TABELA VENDAS CAMPO ID_USUARIO
+ALTER TABLE Tbl_Vendas ADD CONSTRAINT fk_id_Usuario_vendas FOREIGN KEY (fk_id_Usuario) REFERENCES Tbl_Usuario(id_Usuario);
+
+#FOREIGN KEY TABELA QUANTIDADE_PRODUTOS CAMPO ID_VENDA
+ALTER TABLE Tbl_QuantidadeProdutos ADD CONSTRAINT fk_id_Venda_Quantidade FOREIGN KEY (fk_id_Venda) REFERENCES Tbl_Vendas(id_Venda);
+
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
